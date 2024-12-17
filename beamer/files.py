ddef48c7da9eee4ce2ae1eec8b1daddd3e4852cb
J Alan Brogan <github@al-got-rhythm.net>
2024-12-17 23:43:28 +0000

    Try known encodings
    We have seen that "windows-1252" and "utf-8" can be used

--- beamer/files.py
+++ beamer/files.py
@@ -6,6 +6,9 @@ Should be able to read *.csv and *.json files
 import csv
 import json
 
+# Known encodings for data files we have seen
+known_encodings = ["utf-8", "windows-1252"]
+
 
 def read_csv_file(path_to_csv: str) -> list:
     """Read a csv file at that path
@@ -14,17 +17,24 @@ def read_csv_file(path_to_csv: str) -> list:
     """
     if not path_to_csv:
         raise ValueError(f"Empty path to csv file: {path_to_csv!r}")
-    with open(path_to_csv, encoding="windows-1252", errors="replace") as stream:
-        return list(csv.DictReader(stream))
+    for encoding in known_encodings:
+        try:
+            with open(path_to_csv, encoding=encoding) as stream:
+                return list(csv.DictReader(stream))
+        except UnicodeDecodeError:
+            pass
 
 
 def read_json_file(path_to_json: str) -> dict:
     """Read a json file at given path
 
-    Return:
-        Content of json file, as dict
+    Return a dict with the file's data
     """
     if not path_to_json:
         raise ValueError(f"Empty path to json file: {path_to_json!r}")
-    with open(path_to_json, encoding="utf-8", errors="replace") as stream:
-        return json.load(stream)
+    for encoding in known_encodings:
+        try:
+            with open(path_to_json, encoding=encoding) as stream:
+                return json.load(stream)
+        except UnicodeDecodeError:
+            pass
